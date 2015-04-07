7db9a3eb7cd103b88bbfab67a6516c91e2b651de
clee1515 <clee15@punahou.edu>
2015-04-07 13:15:30 -1000

    Update
    I fixed the problem that comes when people type the same letter twice

--- Hangman.java
+++ Hangman.java
@@ -1,71 +1,103 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Hangman
 {
   public static void main(String[] args){
     String playerAnswer = "_ _ _ _ _ ";
-    
+    ArrayList<String> guesses = new ArrayList<String>();
     int lives = 10;
     int right = 0;
-    
+    int past = 0;
+    int amount = 1;
+    Boolean notGuessed = true;
     Scanner input = new Scanner(System.in);
     HangmanAnswer hma = new HangmanAnswer();
+    System.out.print("What is your guess?");
     while (right != 5 && lives != 0)
-    {  
-      System.out.print("What is your guess?");
+    { 
       String guess = input.nextLine();
+      guesses.add(guess);
+      amount++;
       {
-        int found = hma.getAnswer().indexOf(guess);
-        
-        if (found == -1)
+        for (int i = 0; i < guesses.size(); i++)
         {
-          lives--;
-          {
-            if (lives == 1)
-            {
-              System.out.print("Sorry, wrong answer. You have " + lives + " life left. ");
-              System.out.println(playerAnswer);
-            }
-            
-            else if (lives == 0)
-            {
-              System.out.print("You didn't guess the word. It was " + hma.getAnswer() + ".");
-            }
-            
-            else 
-            {
-              System.out.print("Sorry, wrong answer. You have " + lives + " lives left. ");
-              System.out.println(playerAnswer);
-            }
-          }    
-          
-        }
-        else 
-        {
-          right ++;
-          if (right == 5)
-          {
-            playerAnswer = playerAnswer.substring(0, found * 2) + guess + playerAnswer.substring(found * 2 + 1);
-            System.out.print("Good Job!You guessed " + playerAnswer + " correctly!");
-          }  
-          else
-          {
-            if (guess.equals("a") || guess.equals("e") || guess.equals("i") || guess.equals("o") || guess.equals("u"))
-            {
-              System.out.print("There is an " + guess + " in the word. ");
-              playerAnswer = playerAnswer.substring(0, found * 2) + guess + playerAnswer.substring(found * 2 + 1);
-              System.out.print(playerAnswer);
-            }
-            else 
-            {
-              System.out.print("There is a " + guess + " in the word. ");
-              playerAnswer = playerAnswer.substring(0, found * 2) + guess + playerAnswer.substring(found * 2 + 1);
-              System.out.print(playerAnswer);
-            }
+          if (guess.equals(guesses.get(i)))
+          { 
+            past++;
           }
+        }
+        if (past > amount)
+        {
+          notGuessed = false;
+          System.out.print("You already picked " + guess + " ! Pick another.");
+          past = 0;
+          amount = 0;
+        } 
+        else if (notGuessed = true)
+        {                     
+          int found = hma.getAnswer().indexOf(guess);
+          if (found == -1)
+          {
+            lives--;
+            {
+              if (lives == 1)
+              {
+                System.out.print("Sorry, wrong answer. You have " + lives + " life left. ");
+                System.out.println(playerAnswer);
+                System.out.print("What is your next guess?");
+                guesses.add(guess);
+              }
+              else if (lives == 0)
+              {
+                System.out.print("You didn't guess the word. It was " + hma.getAnswer() + ".");
+              }
+              else 
+              {
+                System.out.print("Sorry, wrong answer. You have " + lives + " lives left. ");
+                System.out.println(playerAnswer);
+                System.out.print("What is your next guess?");
+                guesses.add(guess);
+              }
+            }      
+          }
+          else 
+          {
+            right ++;
+            if (right == 5)
+            {
+              playerAnswer = playerAnswer.substring(0, found * 2) + guess + playerAnswer.substring(found * 2 + 1);
+              System.out.print("Good Job!You guessed " + playerAnswer + " correctly!");
+            }  
+            else
+            {
+              if (guess.equals("a") || guess.equals("e") || guess.equals("i") || guess.equals("o") || guess.equals("u"))
+              {
+                System.out.print("Correct, there is an " + guess + " in the word. ");
+                playerAnswer = playerAnswer.substring(0, found * 2) + guess + playerAnswer.substring(found * 2 + 1);
+                System.out.print(playerAnswer);
+                System.out.print("What is your next guess?");
+                guesses.add(guess);
+              }
+              else 
+              {
+                System.out.print("Correct, there is a " + guess + " in the word. ");
+                playerAnswer = playerAnswer.substring(0, found * 2) + guess + playerAnswer.substring(found * 2 + 1);
+                System.out.print(playerAnswer);
+                System.out.print("What is your next guess?");
+                guesses.add(guess);
+              }
+            }
+          }   
         }
       }
     }
   }
 }
+
+
+
+
+
+
 
